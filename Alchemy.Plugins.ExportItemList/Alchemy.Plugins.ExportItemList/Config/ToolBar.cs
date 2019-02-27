f0eab690a7cc66351d148568ea55ed92dbd81ba2
Saurabh Gangwar <sgangwar@starbucks.com>
2019-02-27 23:16:53 +0530

    Restructuring - Original Commit

---
+++ Alchemy.Plugins.ExportItemList/Alchemy.Plugins.ExportItemList/Config/ToolBar.cs
@@ -0,0 +1,46 @@
+﻿using Alchemy.Plugins.ExportItemList;
+using Alchemy4Tridion.Plugins.GUI.Configuration;
+
+namespace Alchemy4Tridion.Plugins.ExportItemList.Config
+{
+    /// <summary>
+    /// This class represnts the RibbonToolbarExtension
+    /// It will add a Button "Export Item List" on Dashboard view under the HomePage tab
+    /// </summary>
+    public class ToolbarButton : RibbonToolbarExtension
+    {
+        public ToolbarButton()
+        {
+            // HTML Id of the button
+            AssignId = @"ExportItemList";
+
+            // Name of the Javascript Command to be fired on the button click
+            Command = @"ExportItemList";
+
+            // Label for the button
+            Name = Resources.ToolbarButtonName;
+
+            // Tool tip for the button
+            Title = Resources.ToolbarButtonTooltip;
+
+            // Ribbon toolbar tab for the button
+            PageId = Constants.PageIds.HomePage;
+
+            // The group inside homepage tab, where we want to show the button
+            GroupId = Constants.GroupIds.HomePage.ManageGroup;
+
+            // Insert the button before "Where Used Button"
+            InsertBefore = @"WhereUsedBtn";
+
+            // Add "ButtonGroup" resource group as a dependancy of the extension
+            Dependencies.Add<ButtonGroup>();
+
+            // Apply the extension to Dashboard view
+            Apply.ToView(Constants.Views.DashboardView, "DashboardToolbar");
+
+            Apply.ToView("*", "Toolbar");
+
+
+        }
+    }
+}
---
+++ Alchemy.Plugins.ExportItemList/Alchemy.Plugins.ExportItemList/Static/Commands/ExportItemListCommand.js
@@ -0,0 +1,116 @@
+﻿Alchemy.command("${PluginName}", "ExportItemList", 
+{
+    
+    //-------------- Function:isAvailable---------
+    // Returns boolean, determins if the context menu option "ExportItemList" associated with the command is available or not.
+    // -------------------------------------------
+	isAvailable: function(selection, pipeline)
+    {
+        return true;
+	},
+    //-------------- Function:isEnabled---------
+    // Returns boolean, determins if the Ribbon Button "ExportItemList" associated with the command is enabled or not.
+    // -------------------------------------------
+	isEnabled: function(selection, pipeline)
+    {
+        return true;
+    },
+    //-------------- Function:init---------
+    // Called when plugin is initialized
+    // Reads the plugin settings and set it inside a global object
+    // -------------------------------------------
+    init: function() {
+
+    },
+    //-------------- Function:execute---------
+    // Called when command is executed
+    // -------------------------------------------
+        execute: function () {
+            // Get the view
+            var tridionView = $display.getView();
+            var xmlDoc;
+            var columns;
+
+            if (tridionView.getId() == "DashboardView") {
+                var filteredListControl = tridionView.getFilteredListControl();
+                var listControl = filteredListControl.getList();
+                xmlDoc = listControl.getContent();
+                columns = ["ID", "Title", "Type", "FromPub", "IsNew", "IsPublished", "Modified", "IsShared", "IsLocalized"];
+            }
+            else if (tridionView.getId() == "PublishQueueView")
+            {
+                var publishTransList = tridionView._getListPublishTransactions();
+                xmlDoc = publishTransList.getXmlDocument();
+                columns = ["ID", "Title", "ItemID", "ItemType", "State", "StateChangeDate", "TargetTypeID", "TargetTypeTitle", "Publication", "PublicationTarget", "ItemPath", "Action", "ScheduleDate", "UserId", "User", "UserDescription", "Priority", "PublisherHost", "IsCompleted", "Managed", "Allow", "Deny", "AllowedActions", "DeniedActions", "RenderingTime", "ResolvingTime", "TotalExecutionTime", "Type"];
+            }
+            
+           
+            if (confirm('Do you want to export?')) {
+                this.covertXMLToCSV(xmlDoc, columns);
+            }
+        },
+
+    //-------------- Function:covertXMLToCSV------
+    // Converts the item list xml to CSV format
+    // -------------------------------------------
+    covertXMLToCSV: function (xmlDoc, columns) {
+        
+        var csv = [];
+
+        var tableHeader = columns.join();
+
+        csv.push(tableHeader);
+        
+        var item = xmlDoc.getElementsByTagName("tcm:Item");
+
+        var i;
+        for (i = 0; i < item.length; i++) {
+            if (item[i].getAttribute("Type") != null) {
+                var rowArray = [];
+                var j;
+                for (j = 0; j < columns.length; j++) {
+                    var attribute = columns[j];
+                    var attributeValue = item[i].getAttribute(attribute);
+                    if (attributeValue != null) { attributeValue = attributeValue.replace(',', '|');}
+                    rowArray.push(attributeValue);
+                }
+                csv.push(rowArray.join());
+            }
+        }
+        if (csv.length > 1) {
+            this.downloadCSV(csv.join("\n"), "ExportItemList.csv");
+        }
+        else {
+            $messages.registerNotification("ExportItemList: There are no items to export.");
+        }
+    },
+    //-------------- Function:downloadCSV------
+    // Downloads the file
+    // -------------------------------------------
+    downloadCSV: function (csv, filename) {
+        var csvFile;
+        var downloadLink;
+
+        // CSV file
+        csvFile = new Blob([csv], { type: "text/csv" });
+
+        // Download link
+        downloadLink = document.createElement("a");
+
+        // File name
+        downloadLink.download = filename;
+
+        // Create a link to the file
+        downloadLink.href = window.URL.createObjectURL(csvFile);
+
+        // Hide download link
+        downloadLink.style.display = "none";
+
+        // Add the link to DOM
+        document.body.appendChild(downloadLink);
+
+        // Click download link
+        downloadLink.click();
+    },
+
+    });
